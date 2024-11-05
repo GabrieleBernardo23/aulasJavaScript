e39cf0b07aa3a861761561324c3fcb6493da7538
Gabriele <gabriele.bernardo.oliveira@gmail.com>
2024-11-05 15:26:56 -0300

    aula 12

---
+++ AULA12/class.js
@@ -0,0 +1,10 @@
+class Livro {  // primeira letra em maiúsculo
+    constructor(autor, editora, paginas, modelo, categoria){
+        this.autor = autor
+        this.editora = editora
+        this.paginas = paginas
+        this.modelo = modelo
+        this.categoria = categoria
+    }
+    
+}
---
+++ AULA12/objeto.js
@@ -0,0 +1,53 @@
+let sapato1 = {
+    marca: 'nike',
+    cadarco: true,
+    lingua: 'G',
+    tamanho: 41,
+    palmilha: 'ortopédica',
+    cor: {
+        tenis: 'preto',
+        cadarco:'azul'
+    }
+}
+
+console.log(sapato1);
+console.log(sapato1.tamanho);
+console.log(sapato1.cor.tenis);
+
+console.log('-------------------------');
+
+let sapato2 = {
+    marca: 'moleca',
+    cadarco: false,
+    lingua: 'M',
+    tamanho: 38,
+    palmilha: 'Normal',
+    cor: {
+        tenis: 'azul',
+        sola: 'branca'
+    }
+}
+
+console.log(sapato2);
+console.log(sapato2.tamanho);
+console.log(sapato2.cor.tenis);
+
+console.log('-------------------------');
+
+let sapato3 = {
+    marca: 'Modare',
+    cadarco: true,
+    lingua: 'M',
+    tamanho: 37,
+    palmilha: 'Normal',
+    cor: {
+        tenis: 'Bege',
+        sola: 'Bege'
+    }
+}
+
+console.log(sapato3);
+console.log(sapato3.tamanho);
+console.log(sapato3.cor.tenis);
+
+console.log('-------------------------');
